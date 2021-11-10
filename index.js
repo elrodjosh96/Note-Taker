31964c9e97182850dcbaf9d121d5150f74738ede
Josh Elrod <elrodjosh96@gmail.com>
2021-11-10 16:48:42 -0500

    finished code

--- index.js
+++ index.js
@@ -1,23 +1,60 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 const PORT = 3001;
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile, writeToFile } = require('./fsUtils');
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});  
+
+app.get('/api/notes', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+app.post('/api/notes', (req, res) =>{
+
+  const {title, text} = req.body;
+
+  if(title && text) {
+
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id !== noteId);
 
 
-// // Write the string to a file
-// fs.writeFile(`./db/db.json`, reviewString, (err) =>
-// err
-//   ? console.error(err)
-//   : console.log(
-//       `Review for ${newReview.product} has been written to JSON file`
-//     )
-// );
+      writeToFile('./db/db.json', result);
+
+      res.json(`Item ${noteId} has been deleted`);
+    });
+});
+
 // 5 routes(including bonus), static folder route
-// fs.writefile in POST route 
 // GET request
 app.get('/api/notes', (req, res) => {
     // Let the client know that their request was received
@@ -46,6 +83,7 @@ app.get('/api/notes', (req, res) => {
     console.log(`Express server listening on port ${PORT}!`)
   );
   
+
 // fs.appendFile.(inside get route)
 // <!-- db.json file, create express server app.get(/api/routes) read file system, present data as response -->
 // <!--  -->
